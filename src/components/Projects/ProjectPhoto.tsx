4ece00e89f27b0a9297d639e3ca7bfb5c5d13e56
olehkulys <164574291+MrSecretest@users.noreply.github.com>
2024-08-24 20:00:16 +0300

    Fix screen sizes, change carousel

--- src/components/Projects/ProjectPhoto.tsx
+++ src/components/Projects/ProjectPhoto.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react';
 import '../../App.css'
 import './ProjectStyles.css';
 import project1 from '../Projects/ProjectsMedia/project1.png';
 import project2 from '../Projects/ProjectsMedia/project2.webm';
-import project2Preview from '../Projects/ProjectsMedia/project2.png';
 import project3 from '../Projects/ProjectsMedia/project3.png';
-import ButtonSquare from "../Buttons/ButtonSquare.tsx";
-import {LazyLoadImage} from "react-lazy-load-image-component";
 
 const projects = [
     { src: project1, alt: 'Project 1', description: 'First Figma design ever', type: 'img'},
@@ -15,33 +11,19 @@ const projects = [
 ];
 
 function ProjectPhoto() {
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const handlePrevClick = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? projects.length - 1 : prevIndex - 1));
-    };
-
-    const handleNextClick = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === projects.length - 1 ? 0 : prevIndex + 1));
-    };
 
     return (
         <div className="image-text-container">
-            <div className="carousel-button left">
-                <ButtonSquare rotationIndex={1} onClick={handlePrevClick}/>
-            </div>
             <div
-                className="image-wrapper"
-                style={{transform: `translateX(-${currentIndex * 100}%)`}}
-            >
+                className="image-wrapper">
                 {projects.map((project, index) => (
 
                     <div key={index} className="image-text-slide">
                         {project.type === 'img'? (
-                            <LazyLoadImage placeholderSrc={project.src} effect={"blur"} className="border" src={project.src} alt={project.alt} />
+                            <img className="border" src={project.src} alt={project.alt} />
                             ):
                             (
-                            <video className="border" src={project.src} autoPlay loop muted poster={project2Preview}>
+                            <video className="border" src={project.src} autoPlay loop muted >
                                 Your browser does not support this video.
                             </video>
                                         )
@@ -52,9 +34,7 @@ function ProjectPhoto() {
                     </div>
                 ))}
             </div>
-            <div className="carousel-button right">
-                <ButtonSquare rotationIndex={3} onClick={handleNextClick}/>
-            </div>
+
         </div>
     );
 }
